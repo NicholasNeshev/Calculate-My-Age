5a3641f4459efdc5dae8d7b13979e9fbf6801a38
Nicholas Neshev <nicholasneshev@hotmail.com>
2018-02-06 23:00:54 -0800

    Calculate My Age

--- calculateAge.js
+++ calculateAge.js
@@ -1,29 +1,29 @@
 function calculateAge() {
-			var age;
-			var elem;
+    var age;
+	var elem;
 						
-			age = document.getElementById("numb").value;
+	age = document.getElementById("numb").value;
 
-			elem = document.getElementById("result");
+	elem = document.getElementById("result");
 	
-			if (age == "" || age < -3 || age > 237) {
-				elem.innerHTML = "Please enter a valid number.";
-				elem.style.color = "#ED1C24";
-			}
-			else{
-				// Converting string to a number
-				age = age * 1;
+	if (age == "" || age < -3 || age > 237) {
+		elem.innerHTML = "Please enter a valid number.";
+		elem.style.color = "#ED1C24";
+	}
+	else{
+		// Converting string to a number
+		age = age * 1;
 				
-				// Decrypting age
-				age = (age + 3) / 2;
+		// Decrypting age
+		age = (age + 3) / 2;
 				
-				if (age < 1) {
-					elem.innerHTML = "You're still a baby!";
-				}
-				else {			
-					elem.innerHTML = "Your age is " + age + "!";
-				}
-	
-				elem.style.color = "lime";
-			}			
+		if (age < 1) {
+			elem.innerHTML = "You're still a baby!";
 		}
+		else {			
+			elem.innerHTML = "Your age is " + age + "!";
+		}
+	
+		elem.style.color = "lime";		
+    }
+}
